b6c5bf5df60808e9e84afea8f02256aace0161cb
Felipe Genuino <design@felipegenuino.com>
2020-04-27 00:45:17 -0300

    start validando data

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import { uuid } from 'uuidv4';
+
+import { startOfHour, parseISO } from 'date-fns';
+
+const appointmentsRouter = Router();
+
+const appointments = [];
+
+appointmentsRouter.post('/', (request, response) => {
+    const { provider, date } = request.body;
+
+    const parsedDate = startOfHour(parseISO(date));
+
+    const appointment = {
+        id: uuid(),
+        provider,
+        parsedDate,
+    };
+
+    appointments.push(appointment);
+
+    return response.json(appointment);
+});
+
+export default appointmentsRouter;
